eee0f2d05ed95657a88f5468fef8f63d381c77d5
DAMS1875 <petersdamon8@gmail.com>
2024-04-30 12:58:59 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const join = document.getElementById("join");
+const money = document.getElementById("money");
+const para = document.getElementById("para")
+const Subscription = document.getElementById("Subscription")
+const number = document.getElementById
+
+function info(){
+    join.textContent = "Join our community";
+    money.textContent = "30-day,hassle-free money guarantee";
+    para.textContent = "Gain access to our full library of tutorials along with expert code reviews. Perfect for any developer who are serious about honing their skills. ";
+    Subscription.textContent = "Monthy Subscription"
+}
+info();
